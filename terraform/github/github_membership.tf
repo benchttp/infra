a264964d6a788085c65697dcee49bb561b5f446c
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-01-16 16:21:03 +0100

    Set up github_membership

---
+++ terraform/github/github_membership.tf
@@ -0,0 +1,29 @@
+resource "github_membership" "alex_mongeot" {
+  username = "alex-mngt"
+  role     = "admin"
+}
+
+resource "github_membership" "clara_gonnon" {
+  username = "c1-ra"
+  role     = "admin"
+}
+
+resource "github_membership" "damien_mathieu" {
+  username = "damien-mathieu"
+  role     = "admin"
+}
+
+resource "github_membership" "gregory_albouy" {
+  username = "gregoryalbouy"
+  role     = "admin"
+}
+
+resource "github_membership" "kerian_pelat" {
+  username = "nartydev"
+  role     = "admin"
+}
+
+resource "github_membership" "thomas_moreira" {
+  username = "moreirathomas"
+  role     = "admin"
+}
