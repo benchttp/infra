d3502d2538b27b92bc39bed64ae43dd17bf49b98
Gregory Albouy <gregoryalbouy75@gmail.com>
2022-02-19 20:54:43 +0100

    github: move main into github dir

---
+++ terraform/github/main.tf
@@ -0,0 +1,11 @@
+variable "github_token" {
+  type = string
+}
+
+variable "github_webhook_activity_url" {
+  type = string
+}
+
+variable "github_webhook_comments_url" {
+  type = string
+}
--- terraform/github/variables.tf
+++ terraform/github/variables.tf
@@ -1,15 +1,3 @@
-variable "github_token" {
-  type = string
-}
-
-variable "github_webhook_activity_url" {
-  type = string
-}
-
-variable "github_webhook_comments_url" {
-  type = string
-}
-
 locals {
   repositories = {
     "infra" = {
